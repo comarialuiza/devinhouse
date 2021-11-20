2619281ebd6d7716cb929acac499a311bd5e54d0
Maria Luiza Coelho <maria.luiza@way2.com.br>
2021-11-19 21:49:59 -0300

    solves exercises

--- semana05/aula03/ex02_res.js
+++ semana05/aula03/ex02_res.js
@@ -1,21 +1,45 @@
-class Person {
-    constructor(name, yearBirth) {
-        this.name = name;
-        this.yearBirth = yearBirth;
+class Book {
+    constructor(title, author, year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.loaned = false;
     }
 
-    calculateAge() {
-        return 2021 - this.yearBirth;
+    loanBook() {
+        if (this.loaned === true) {
+            return 'Esse livro já está emprestado';
+        } else {
+            this.loaned = true;
+            return 'Livro emprestado';
+        }
+    }
+
+    returnBook() {
+        if (this.loaned === false) {
+            return 'Esse livro não está emprestado';
+        } else {
+            this.loaned = false;
+            return 'Livro devolvido';
+        }
+    }
+
+    static sortBookByDate(booksArray) {
+        const sortedBooksArray = booksArray.sort((book1, book2) => {
+            if (book1.year !== book2.year) return book1.year - book2.year;
+        });
+
+        return sortedBooksArray;
     }
 }
 
-class Programmer extends Person {
-    constructor(name, yearBirth, language) {
-        super(name, yearBirth);
-        this.language = language;
-    }
+const twilight = new Book('Twilight', 'Stephenie Meyer', 2005);
+const lordOfTheRings = new Book('Lord of the Rings', 'J.R.R. Tolkien', 1954);
+const lordOfTheRings2 = new Book('Lord of the Rings', 'J.R.R. Tolkien', 1954);
+const harryPotter = new Book('Harry Potter and the Philosopher\'s stone', 'J.K. Rowking', 1997);
 
-    greet() {
-        return `My name is ${ this.name } and I program in ${ language }!`;
-    }
-}
+const booksArray = [twilight, lordOfTheRings, harryPotter, lordOfTheRings2];
+// console.log(Book.sortBookByDate(booksArray));
+
+console.log(twilight instanceof Book);
+console.log({ title: 'Livro1', author: 'Autor', year: 1950, loaned: false } instanceof Book);
--- semana05/aula04/ex01_res.js
+++ semana05/aula04/ex01_res.js
@@ -1,42 +1,112 @@
 class Book {
+    #title
+    #author
+    #year
+    #loaned
+
     constructor(title, author, year) {
-        this.title = title;
-        this.author = author;
-        this.year = year;
-        this.loaned = false;
+        this.#title = title;
+        this.#author = author;
+        this.#year = year;
+        this.#loaned = false;
+    }
+
+    get title() {
+        return this.#title;
+    }
+
+    get author() {
+        return this.#author;
+    }
+
+    get year() {
+        return this.#year;
+    }
+
+    get loaned() {
+        return this.#loaned;
     }
 
     loanBook() {
-        this.loaned = true;
+        if (this.#loaned === true) {
+            return 'Esse livro já está emprestado';
+        } else {
+            this.#loaned = true;
+            return 'Livro emprestado';
+        }
     }
 
     returnBook() {
-        this.loaned = false;
+        if (this.#loaned === false) {
+            return 'Esse livro não está emprestado';
+        } else {
+            this.#loaned = false;
+            return 'Livro devolvido';
+        }
     }
 
-    static sortBooksByDate(booksArray) {
-        const sortedBooksArray = booksArray.sort((a, b) => {
-            if (a.year !== b.year) return a.year - b.year;
+    toJSON() {
+        return {
+            title: this.#title,
+            author: this.#author,
+            year: this.#year,
+            loaned: this.#loaned
+        }
+    }
+
+    static sortBookByDate(booksArray) {
+        const sortedBooksArray = booksArray.sort((book1, book2) => {
+            if (book1.year !== book2.year) return book1.year - book2.year;
         });
 
-        return sortedBooksArray;
+        return sortedBooksArray.map(arr => arr.toJSON());
     }
 }
 
 class HQ extends Book {
+    #illustrator
+
     constructor(title, author, year, illustrator) {
         super(title, author, year);
-        this.illustrator = illustrator;
+        this.#illustrator = illustrator;
+    }
+
+    get illustrator() {
+        return this.#illustrator;
+    }
+
+    toJSON() {
+        return {
+            title: this.title,
+            author: this.author,
+            year: this.year,
+            loaned: this.loaned,
+            illustrator: this.illustrator
+        }
     }
 }
 
-const twilight = new Book("Twilight", "Stephenie Meyer", 2005);
-twilight.loanBook();
-twilight.loaned = false;
-console.log(twilight);
+const twilight = new Book('Twilight', 'Stephenie Meyer', 2005);
+const lordOfTheRings = new Book('Lord of the Rings', 'J.R.R. Tolkien', 1954);
+const harryPotter = new Book('Harry Potter and the Philosopher\'s stone', 'J.K. Rowking', 1997);
 
-const trialsOfApollo = new Book("The Tower of Nero", "Rick Riordan", 2020);
-const umbrellaAcademy = new HQ("Umbrella Academy", "Gerard Way", 2008, "Gabriel Bá");
+const booksArray = [twilight, lordOfTheRings, harryPotter];
+// console.log(Book.sortBookByDate(booksArray));
 
-const sortedBooks = Book.sortBooksByDate([twilight, trialsOfApollo, umbrellaAcademy]);
-// console.log('sortedBooks', sortedBooks);
+const umbrellaAcademy = new HQ('Umbrella Academy', 'Gerard Way', 2008, 'Gabriel Bá');
+const scottPilgrim = new HQ('Scott Pilgrim vs. the World', 'Lee O Maley', 2004, 'Lee O Maleu');
+const watchmen = new HQ('Watchmen', 'Alan Moore', 1986, 'Dave Gibbons');
+
+const hqArray = [umbrellaAcademy, scottPilgrim, watchmen];
+
+const multiArray = [twilight, lordOfTheRings, harryPotter, umbrellaAcademy, scottPilgrim, watchmen];
+// console.log(Book.sortBookByDate(multiArray));
+console.log(HQ.sortBookByDate(multiArray));
+
+// console.log(HQ.sortBookByDate(hqArray));
+
+// console.log(twilight.toJSON());
+// console.log(umbrellaAcademy.toJSON());
+
+// twilight.title = 'New Moon';
+// console.log(twilight.toJSON());
