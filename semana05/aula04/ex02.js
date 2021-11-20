08b76a58f94f121fd445df4645a4c103d54f2b6b
deywid <90353843+deywid@users.noreply.github.com>
2021-11-20 19:44:00 -0300

    bugfix proposal
    [changelog]
    
    dynamically calculating $age property
    class Students && class Teacher now extends from class Person
    calculateAverageGrade() method now returns a float value
    class Student now has Its own sayHello() && sayGoodbye() methods
    fixed $subject entry value in $teacher new instance of class Teacher
    encapsulated all properties from each class
    created a new method toJSON() allowing to print private properties by accessing get methods

--- semana05/aula04/ex02.js
+++ semana05/aula04/ex02.js
@@ -1,45 +1,127 @@
 class Person {
+  #name
+  #birthYear
+  #age
+
   constructor(name, birthYear, interests) {
-    this.name = name;
-    this.birthYear = birthYear;
-    this.age = 2021 - birthYear;
-    this.interests = interests;
+    this.#name = name;
+    this.#birthYear = birthYear;
+    this.#age = new Date().getFullYear() - birthYear;
+    this._interests = interests;
   }
-  
+
+  get name() {
+    return this.#name
+  }
+
+  get birthYear() {
+    return this.#birthYear
+  }
+
+  get age() {
+    return this.#age
+  }
+
+  get interests() {
+    return this._interests
+  }
+
+  set interests(newInterest) {
+    this._interests = newInterest
+  }
+
   sayHello() {
-    return `Hello! My name is ${ this.name } and I am interested in ${ this.interests.join(', ') }.`
+    return `Hello! My name is ${this.name} and I am interested in ${this.interests.join(', ')}.`
   }
-  
+
   sayGoodbye() {
     return `Goodbye!`;
   }
+
+  toJSON() {
+    return { // call to getters and setters
+      name: this.name,
+      birthYear: this.birthYear,
+      age: this.age,
+      interests: this.interests
+    }
+  }
 }
 
-class Teacher {
+class Teacher extends Person {
+  #subject
   constructor(name, birthYear, interests, subject) {
     super(name, birthYear, interests);
-    this.subject = subject;
+    this.#subject = subject;
   }
-  
+
   sayHello() {
-    return `Hello! My name is ${ this.name } and I teach ${ subject }`;
+    return `Hello! My name is ${this.name} and I teach ${this.subject}`;
+  }
+
+  get subject(){
+    return this.#subject
+  }
+
+  toJSON() {
+    return { // call to getters and setters
+      name: this.name,
+      birthYear: this.birthYear,
+      age: this.age,
+      interests: this.interests,
+      subject: this.subject
+    }
   }
 }
 
-class Student {
+class Student extends Person {
   constructor(name, birthYear, interests, grades) {
     super(name, birthYear, interests);
-    this.grades = grades;
+    this._grades = grades;
   }
-  
+
   calculateAverageGrade() {
-    return Math.round((this.grades.reduce((sum, curGrade) => sum + curGrade)) / this.grades.length);
+    return Math.fround((this.grades.reduce((sum, curGrade) => sum + curGrade)) / this.grades.length).toFixed(2);
+  }
+
+  sayHello() {
+    return `Hello! My name is ${this.name} and I expect you to not be worried about my grades: ${this.grades}`;
+  }
+
+  get grades(){
+    return this._grades
+  }
+
+  set grades(newGrade) {
+    this._grades = newGrade
+  }
+
+  toJSON() {
+    return { // call to getters and setters
+      name: this.name,
+      birthYear: this.birthYear,
+      age: this.age,
+      interests: this.interests,
+      grade: this.grades
+    }
   }
 }
 
-const student = new Student("Joana", 1985, ['reading', 'watching movies'], [9, 3, 10]);
-console.log(student.calculateAverageGrade());
-console.log(student.sayHello());
+const person = new Person("Deywid", 1991, ['programming', 'playing drums']);
+console.log(person.sayHello());
+person.interests.push('football')
+console.log(person.toJSON());
+console.log(person.sayGoodbye());
 
-const teacher = new Teacher("Marcus", 1972, ['yoga', 'cooking', 'math']);
+const student = new Student("Joana", 1985, ['reading', 'watching movies'], [9, 3, 10]);
+console.log(student.sayHello());
+console.log(student.calculateAverageGrade());
+student.grades.push(8);
+console.log(student.calculateAverageGrade());
+console.log(student.toJSON());
+console.log(student.sayGoodbye());
+
+const teacher = new Teacher("Marcus", 1972, ['yoga', 'cooking'], 'math');
 console.log(teacher.sayHello());
+console.log(teacher.toJSON());
+console.log(teacher.sayGoodbye());
