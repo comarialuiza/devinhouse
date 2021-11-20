1607a0c67f38d5a3dfc1e1a7dbb32b6f80557bdb
andreMotta22 <git config --global user.email andre_l_motta@estudante.sesisenai.org.br>
2021-11-19 23:05:06 -0300

    bug resolvido

--- semana05/aula04/ex02.js
+++ semana05/aula04/ex02.js
@@ -1,45 +1,109 @@
 class Person {
+  #name;
+  #birthYear;
+  #interests;
+
   constructor(name, birthYear, interests) {
     this.name = name;
     this.birthYear = birthYear;
-    this.age = 2021 - birthYear;
+    this.age = new Date().getFullYear() - birthYear;
     this.interests = interests;
   }
-  
+
   sayHello() {
-    return `Hello! My name is ${ this.name } and I am interested in ${ this.interests.join(', ') }.`
+    return `Hello! My name is ${
+      this.name
+    } and I am interested in ${this.interests.join(", ")}.`;
   }
-  
+
   sayGoodbye() {
     return `Goodbye!`;
   }
+  //  ============ set =============
+  set name(valor) {
+    this.#name = valor;
+  }
+  set birthYear(valor) {
+    this.#birthYear = valor;
+  }
+  set interests(valor) {
+    this.#interests = valor;
+  }
+
+  //  ============ get =============
+  get name() {
+    return this.#name;
+  }
+  get birthYear() {
+    return this.#birthYear;
+  }
+  get interests() {
+    return this.#interests;
+  }
 }
 
-class Teacher {
+class Teacher extends Person {
+  #subject;
   constructor(name, birthYear, interests, subject) {
     super(name, birthYear, interests);
-    this.subject = subject;
+    this.#subject = subject;
   }
-  
   sayHello() {
-    return `Hello! My name is ${ this.name } and I teach ${ subject }`;
+    return `Hello! My name is ${this.name} i have ${
+      this.age
+    } years and I teach ${
+      this.subject
+    } and I am interested in ${this.interests.join(", ")}`;
+  }
+  set subject(valor) {
+    this.#subject = valor;
+  }
+  get subject() {
+    return this.#subject;
   }
 }
 
-class Student {
+class Student extends Person {
+  #grades;
   constructor(name, birthYear, interests, grades) {
     super(name, birthYear, interests);
-    this.grades = grades;
+    this.#grades = grades;
   }
-  
+
   calculateAverageGrade() {
-    return Math.round((this.grades.reduce((sum, curGrade) => sum + curGrade)) / this.grades.length);
+    return Math.round(
+      this.#grades.reduce((sum, curGrade) => sum + curGrade) /
+        this.#grades.length
+    );
+  }
+  sayHello() {
+    return `Hello! My name is ${this.name}. My age is ${
+      this.age
+    } , and my average grade is ${this.calculateAverageGrade()}. I am interested in ${this.interests.join(
+      ", "
+    )}.`;
+  }
+  set grades(valor) {
+    this.#grades = valor;
+  }
+  get grades() {
+    return this.#grades;
   }
 }
 
-const student = new Student("Joana", 1985, ['reading', 'watching movies'], [9, 3, 10]);
-console.log(student.calculateAverageGrade());
+const student = new Student(
+  "Joana",
+  1985,
+  ["reading", "watching movies"],
+  [9, 3, 10]
+);
+// console.log(student.calculateAverageGrade());
 console.log(student.sayHello());
 
-const teacher = new Teacher("Marcus", 1972, ['yoga', 'cooking', 'math']);
+const teacher = new Teacher(
+  "Marcus",
+  1972,
+  ["yoga", "cooking", "math"],
+  "matematica"
+);
 console.log(teacher.sayHello());
