0824355e0b12a3d6876fafa86cc1b2fd79060efb
Maria Luiza Coelho <maria.luiza@way2.com.br>
2021-11-22 21:06:41 -0300

    creates first example using import and export

--- semana06/aula03/ex01/index.js
+++ semana06/aula03/ex01/index.js
@@ -1,4 +1,3 @@
 import { nome } from './app.js';
 
 document.getElementById("nomeusuario").innerHTML = nome ? nome : "desconhecido";
-document.getElementById("saudacao").addEventListener("click", digaOla);
